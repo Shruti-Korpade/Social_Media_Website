05f9fd69a2d207836f9f38b69f4c4572bd7b03c6
Shruti Korpade <shruti.korpade21@vit.edu>
2024-10-03 03:19:41 +0530

    first commit

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData= [
+    {
+      name: "Minions",
+      shares: 97,
+    },
+    {
+      name: "Avengers",
+      shares: 80.5,
+    },
+    {
+      name: "SHR Media",
+      shares: 75.5,
+    },
+    {
+      name: "React.js",
+      shares: 72,
+    },
+    {
+      name: "Elon Musk",
+      shares: 71.9,
+    },
+    {
+      name: "Japan",
+      shares: 20,
+    },
+  ];
+  
